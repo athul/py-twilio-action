2546169022fa1f77dff8d8bf192ca6247b474ff9
athul <athul8720@gmail.com>
2020-04-15 21:16:17 +0530

    New Updates on Feedback

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
-FROM python:3.7-slim
-ADD . /app
-WORKDIR /app
-RUN pip install --target=/app twilio
-RUN chmod +x /app/app.py
-CMD ["python", "/app/app.py"]
+FROM python:latest
+RUN pip install twilio
+CMD ["python", "/app.py"]
--- app.py
+++ app.py
@@ -4,35 +4,35 @@ import os
 title = os.getenv("INPUT_IUTITLE")
 num = os.getenv("INPUT_IUNUM")
 actor = os.getenv('GITHUB_ACTOR')
-accountSid = os.getenv('TWILIO_ACCOUNT_SID')
-atoken = os.getenv('TWILIO_AUTH_TOKEN')
 event = os.getenv("GITHUB_EVENT_NAME")
 repo = os.getenv("GITHUB_REPOSITORY")
-state=os.getenv('INPUT_EVE_STATE')
-from_num=os.getenv('INPUT_FROM_NUM')
-to_num=os.getenv('INPUT_TO_NUM')
+action = os.getenv('INPUT_EVENT_ACTION')
+from_num = os.getenv('INPUT_FROM_NUM')
+to_num = os.getenv('INPUT_TO_NUM')
 if title is None:
     title = os.getenv('INPUT_PRTITLE')
 if num is None:
     num = os.getenv('INPUT_PRNUM')
 # ---------- Here we want to check for the responses ---------------
-
-if event=="pull_request":
-    if state=="opened":
-        response=f'A new Pull Request (#{num}) is opened in {repo} by {actor} with title: \"{title}\"'
-    elif state=='closed':
-        response=f'The Pull Request (#{num}) on {repo} is closed by {actor}'
-elif event=='issues':
-    if state=='opened':
-        response=f'A new Issue (#{num}) is opened in {repo} by {actor} with title: \"{title}\"'
-    elif state=='closed':
-        response=f'The Issue (#{num}) on {repo} is closed by {actor}'
+if event == "pull_request":
+    github_url = f'https://github.com/{repo}/pulls/{num}'
+    if action == "opened":
+        response = f'A new Pull Request (#{num}) is opened in {repo} by {actor} with title: \"{title}\" URL: {github_url}'
+    elif action == 'closed':
+        response = f'The Pull Request (#{num}) on {repo} is closed by {actor}, URL: {github_url}'
+elif event == 'issues':
+    github_url = f'https://github.com/{repo}/issues/{num}'
+    if action == 'opened':
+        response = f'A new Issue (#{num}) is opened in {repo} by {actor} with title: \"{title}\" URL: {github_url}'
+    elif action == 'closed':
+        response = f'The Issue (#{num}) on {repo} is closed by {actor}, URL: {github_url}'
 else:
-    response=f'Unknown Event is trigerred'
+    github_url = f'https://github.com/{repo}'
+    response = f'A new {event} has occured in the {repo} by {actor},URL: {github_url}'
 
 # ------------ Starting the client -------------
-client=Client(accountSid,atoken) 
+client = Client()
 print(response)
 print('Sending Message')
-message = client.messages.create(body=response,from_=from_num,to=to_num)
+message = client.messages.create(body=response, from_=from_num, to=to_num)
 print('Message Sent')
